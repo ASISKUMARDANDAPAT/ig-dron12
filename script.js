1881101236e3c5f8bf1d116a0961725e7ec62674
ASIS KUMAR DANDAPAT <147532035+ASISKUMARDANDAPAT@users.noreply.github.com>
2024-10-23 07:49:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript to handle smooth scrolling between sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetSection = document.querySelector(this.getAttribute('href'));
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
